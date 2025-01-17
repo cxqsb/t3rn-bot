a960e1353c4eeb2dacb81b69885118fbf163ff0a
cxqsb <508951305@qq.com>
2025-01-17 07:59:04 +0200

    Add files via upload

---
+++ data_bridge.py
@@ -0,0 +1,9 @@
+# data_bridge.py
+
+data_bridge = {
+    # Data bridge Base
+    "Blast - OP Sepolia": '',
+
+    # Data bridge OP Sepolia
+    "OP - Blast": '',
+}
---
+++ network_config.py
@@ -0,0 +1,15 @@
+# network_config.py
+
+networks = {
+    'Blast': {
+        'rpc_url': 'https://sepolia.blast.io', 
+        'chain_id': 168587773, 
+        'contract_address': 
+        '0x1D5FD4ed9bDdCCF5A74718B556E9d15743cB26A2' 
+    },
+    'OP Sepolia': {
+        'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
+        'chain_id': 11155420,
+        'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
+    },
+}
