1e21fc7d59754524e60669de812274d65624665e
cxqsb <508951305@qq.com>
2025-01-29 13:13:48 +0200

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -2,13 +2,13 @@
 
 networks = {
     'Blast': {
-        'rpc_url': 'https://endpoints.omniatech.io/v1/blast/sepolia/public', 
+        'rpc_url': 'https://fragrant-quiet-spring.blast-sepolia.quiknode.pro/57781984326878d3934b9674cdb47c05f63b65f6', 
         'chain_id': 168587773, 
         'contract_address': 
         '0x1D5FD4ed9bDdCCF5A74718B556E9d15743cB26A2' 
     },
     'OP Sepolia': {
-        'rpc_url': 'https://sepolia.optimism.io',
+        'rpc_url': 'https://fittest-clean-morning.optimism-sepolia.quiknode.pro/fa7496b9e10515665cf724e299cda1fceb88a625',
         'chain_id': 11155420,
         'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
     },
