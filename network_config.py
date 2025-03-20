8f53917fb4518a2657e96d957de4c0f8291e46d0
cxqsb <508951305@qq.com>
2025-03-20 10:06:18 +0200

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -8,7 +8,7 @@ networks = {
         '0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3' 
     },
     'OP Sepolia': {
-        'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
+        'rpc_url': 'https://fittest-clean-morning.optimism-sepolia.quiknode.pro/fa7496b9e10515665cf724e299cda1fceb88a625',
         'chain_id': 11155420,
         'contract_address': '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
     },
