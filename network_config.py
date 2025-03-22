f2472e10ff1e77692a421b37763e1cbe3e25e907
cxqsb <508951305@qq.com>
2025-03-22 16:21:19 +0200

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -2,13 +2,13 @@
 
 networks = {
     'Base': {
-        'rpc_url': 'https://cosmological-tiniest-silence.base-sepolia.quiknode.pro/c08152d36d9be6bf3fa6cd4a5f12da64e620aa88', 
+        'rpc_url': 'https://base-sepolia.g.alchemy.com/v2/Xzjbi3vjyMko1hApZ4ctPWJUFAPoxK1K', 
         'chain_id': 84532, 
         'contract_address': 
         '0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3' 
     },
     'OP Sepolia': {
-        'rpc_url': 'https://fittest-clean-morning.optimism-sepolia.quiknode.pro/fa7496b9e10515665cf724e299cda1fceb88a625',
+        'rpc_url': 'https://opt-sepolia.g.alchemy.com/v2/Xzjbi3vjyMko1hApZ4ctPWJUFAPoxK1K',
         'chain_id': 11155420,
         'contract_address': '0xb6Def636914Ae60173d9007E732684a9eEDEF26E'
     },
