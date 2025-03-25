56c65d484a2dc9ce561e73293f180ff482dbb120
cxqsb <508951305@qq.com>
2025-03-25 17:09:03 +0200

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -5,7 +5,7 @@ networks = {
         'rpc_url': 'https://base-sepolia.g.alchemy.com/v2/Xzjbi3vjyMko1hApZ4ctPWJUFAPoxK1K', 
         'chain_id': 84532, 
         'contract_address': 
-        '0xcee0372632a37ba4d0499d1e2116ecff3a17d3c3' 
+        '0xCEE0372632a37Ba4d0499D1E2116eCff3A17d3C3' 
     },
     'OP Sepolia': {
         'rpc_url': 'https://opt-sepolia.g.alchemy.com/v2/Xzjbi3vjyMko1hApZ4ctPWJUFAPoxK1K',
